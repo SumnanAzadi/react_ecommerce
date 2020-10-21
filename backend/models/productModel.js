675542722372b3a057f67903236fc18eb1868aa1
SumnanAzadi <anadaplay@gmail.com>
2020-10-21 17:05:56 +0600

    mongodb react-router load-sample-data postman middleware

---
+++ backend/models/productModel.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    Comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    rating: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    count: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true, //for created_at and updated_at field
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
-
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT;
 const ENV = process.env.NODE_ENV;
 app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`));
